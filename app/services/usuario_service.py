6d2b133ba74cd7c9c1083096d729c0b03b30deb7
luizacamposss <mluizafields@gmail.com>
2024-11-15 17:22:05 +0000

    agora ta certo

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -5,22 +5,22 @@ class UsuarioService:
     def __init__(self, respository: UsuarioRepository):
         self.repository = respository
 
-    def criar_usuario(self, nome: str, email: str: senha: str):
-    try:
-        usuario = Usuario(nome=nome, email=email, senha=senha)
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try: 
+            usuario = Usuario(nome=nome, email=email, senha=senha)
 
-        usuario_cadastrado = self.repository.pesquisar_usuario_por_email(usuario.email)
+            usuario_cadastrado = self.repository.pesquisar_usuario_por_email(usuario.email)
 
-        if usuario_cadastrado:
-            print("Usuário já cadastrado!")
-            return
-        
-        self.repository.salvar_usuario(usuario)
-        print("Usuário usuario_cadastrado com sucesso!")
-    except TypeError as erro:
-        print(f"Erro ao salvar usuário: {erro}")
-    except Exception as erro:
-        print(f"Ocorreu um erro inesperado: {erro}")
+            if usuario_cadastrado:
+                print("Usuário já cadastrado!")
+                return
+            
+            self.repository.salvar_usuario(usuario)
+            print("Usuário cadastrado com sucesso!")
+        except TypeError as erro: 
+            print(f"Erro ao salvar usuário: {erro}")
+        except Exception as erro: 
+            print(f"Ocorreu um erro inesperado: {erro}")
 
-def listar_todos_usuarios(self):
-    return self.repository.listar_usuarios()
+    def listar_todos_usuarios(self):
+        return self.repository.listar_usuarios()
